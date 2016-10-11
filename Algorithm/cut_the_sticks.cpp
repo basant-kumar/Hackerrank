c1a72cbce39160d0945537525613afd004b18f24
basant-kumar <basantkmeena@gmail.com>
2016-10-11 17:39:23 +0530

    cut the sticks

--- Algorithm/cut_the_sticks.cpp
+++ Algorithm/cut_the_sticks.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
-    int n;
-    vector<int> a[n];
-    for(int i=0;i<n;++i)
-        cin>>(a.push_back(i));
-    cout<<a.size()<<endl;
-
+    int n,z;
+    cin>>n;
+    vector<int> a;
+    for(int i=0;i<n;++i){
+        cin>>z;
+        a.push_back(z);
+    }
+    while(a.size()!=0){
+        int size=a.size();
+        cout<<size<<endl;
+        int min=*(min_element(a.begin(),a.end()));
+        for(int i=0;i<size;++i){
+            a[i]=a[i]-min;
+            if(a[i]==0){
+                a.erase((a.begin()+i));
+                size--;
+                i--;
+            }
+        }
+    }
+    
     return 0;
 }
